71db2a41b0ce182765647159a6b5373ba89ae228
Eddie Zaneski <eddiezane@gmail.com>
2013-09-28 20:47:25 -0400

    Update my moves yo

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var app = express();
 var http = require('http');
 var path = require('path');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var routes = require('./routes');
+var user = require('./routes/user');
 
-var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,6 +31,10 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function(socket) {
+
 });
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,49 +1,21 @@
-Crafty.init(600, 300);
+Crafty.init(400, 400);
 Crafty.background('rgb(127,127,127)');
 
-//Paddles
-Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+var count = 0;
+var loc= {
+  x: null,
+  y: null
+}
+
+var player = Crafty.e("Paddle, 2D, DOM, Color, Fourway")
   .color('rgb(255,0,0)')
-  .attr({ x: 20, y: 100, w: 100, h: 100 })
-  .multiway(4, { A: 180, D: 0, W: -90, S: 90 });
-Crafty.e("Paddle, 2D, DOM, Color, Multiway")
-  .color('rgb(0,255,0)')
-  .attr({ x: 580, y: 100, w: 10, h: 100 })
-  .multiway(4, { RIGHT_ARROW: 0, UP_ARROW: -90, DOWN_ARROW: 90, LEFT_ARROW: 180 });
+  .attr({ x: 100, y: 100, w: 100, h: 100 })
+  .fourway(4)
+  .bind('Move', function(e) {
+    loc.x = this._x;
+    loc.y = this._y;
+  });
 
-//Ball
-Crafty.e("2D, DOM, Color, Collision")
-  .color('rgb(0,0,255)')
-  .attr({ x: 300, y: 150, w: 10, h: 10, 
-      dX: Crafty.math.randomInt(2, 5), 
-      dY: Crafty.math.randomInt(2, 5) })
-  .bind('EnterFrame', function () {
-    //hit floor or roof
-    if (this.y <= 0 || this.y >= 290)
-      this.dY *= -1.02;
-
-    if (this.x > 600) {
-      this.x = 300;
-      Crafty("LeftPoints").each(function () { 
-        this.text(++this.points + " Points") });
-    }
-    if (this.x < 10) {
-      this.x = 300;
-      Crafty("RightPoints").each(function () { 
-        this.text(++this.points + " Points") });
-    }
-
-    this.x += this.dX;
-    this.y += this.dY;
-  })
-  .onHit('Paddle', function () {
-  this.dX *= -1.01;
-})
-
-//Score boards
-Crafty.e("LeftPoints, DOM, 2D, Text")
-  .attr({ x: 20, y: 20, w: 100, h: 20, points: 0 })
-  .text("0 Points");
-Crafty.e("RightPoints, DOM, 2D, Text")
-  .attr({ x: 515, y: 20, w: 100, h: 20, points: 0 })
-  .text("0 Points");
+var update = setInterval(function() {
+  console.log(loc);
+}, 100);
